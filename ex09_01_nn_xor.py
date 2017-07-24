455d18ac1f23ab80e7112b570e057aa844250f2f
HardPlant <abc7988se@naver.com>
2017-07-24 08:07:26 -0700

    relu... not this :(

--- ex09_01_nn_xor.py
+++ ex09_01_nn_xor.py
@@ -11,7 +11,6 @@ layer1 = tf.sigmoid(tf.matmul(X,W1) + b1)
 
 W2 = tf.Variable(tf.random_normal([2,1]), name='weight2')
 b2 = tf.Variable(tf.random_normal([1], name='bias2'))
-
 hypothesis = tf.sigmoid(tf.matmul(layer1, W2)+b2)
 
 cost = -tf.reduce_mean(Y*tf.log(hypothesis)+(1-Y)*tf.log(1-hypothesis))
@@ -20,11 +19,19 @@ train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 predicted = tf.cast(hypothesis > 0.5, dtype = tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype = tf.float32))
 
+#Tensorboard
+w2_hist = tf.summary.histogram("weights2", W2)
+cost_summ = tf.summary.scalar("cost", cost)
+summary = tf.summary.merge_all()
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter('.logs')
+    writer.add_graph(sess.graph)
 
     for step in range(10001):
-        sess.run(train, feed_dict={X:x_data, Y:y_data})
+        s, _ = sess.run([summary,train], feed_dict={X:x_data, Y:y_data})
+        writer.add_summary(s, global_step=10001)
         if step%100 == 0:
             print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run([W1,W2]))
 
--- ex09_03_deep_mnist.py
+++ ex09_03_deep_mnist.py
@@ -14,15 +14,15 @@ Y = tf.placeholder(tf.float32, [None, nb_classes])
 w1 = tf.Variable(tf.random_normal([784, 10]))
 b1 = tf.Variable(tf.random_normal([10]))
 
-layer1 = tf.nn.sigmoid(tf.matmul(X,w1)+b1)
+layer1 = tf.nn.relu(tf.matmul(X,w1)+b1)
 w2 = tf.Variable(tf.random_normal([10, 10]))
 b2 = tf.Variable(tf.random_normal([10]))
 
-layer2 = tf.nn.sigmoid(tf.matmul(layer1,w2)+b2)
+layer2 = tf.nn.relu(tf.matmul(layer1,w2)+b2)
 w3 = tf.Variable(tf.random_normal([10, 10]))
 b3 = tf.Variable(tf.random_normal([10]))
 
-layer3 = tf.nn.sigmoid(tf.matmul(layer2,w3)+b3)
+layer3 = tf.nn.relu(tf.matmul(layer2,w3)+b3)
 w4 = tf.Variable(tf.random_normal([10, nb_classes]))
 b4 = tf.Variable(tf.random_normal([nb_classes]))
 
