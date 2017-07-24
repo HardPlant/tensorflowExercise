48f9a6dccef9b9471b3ac0925854ea4d0a3235d1
HardPlant <abc7988se@naver.com>
2017-07-24 06:23:17 -0700

    neural net! but wrong?

--- ex09_00_nn_xor_lr.py
+++ ex09_00_nn_xor_lr.py
@@ -5,7 +5,7 @@ y_data = np.array([[0],[1],[1],[0]], dtype=np.float32)
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
-W = tf.Variable(tf.random_normal([2,1]), name='weight')
+W = tf.Variable(tf.random_normal([2,2]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
 hypothesis = tf.sigmoid(tf.matmul(X,W)+b)
