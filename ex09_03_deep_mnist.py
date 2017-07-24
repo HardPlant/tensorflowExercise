975ad1ace6717ed93a7df2e1dc13eff017853ff5
HardPlant <abc7988se@naver.com>
2017-07-24 06:43:40 -0700

    deep..but it wrong :<

---
+++ ex09_03_deep_mnist.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+#will automatically download datasets
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+
+nb_classes = 10
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, nb_classes])
+
+W1 = tf.Variable(tf.random_normal([784, 10]))
+b1 = tf.Variable(tf.random_normal([10]))
+
+layer1 = tf.nn.softmax(tf.matmul(X,W1)+b1)
+W2 = tf.Variable(tf.random_normal([10, 10]))
+b2 = tf.Variable(tf.random_normal([10]))
+
+layer2 = tf.nn.softmax(tf.matmul(layer1,W2)+b2)
+W3 = tf.Variable(tf.random_normal([10, 10]))
+b3 = tf.Variable(tf.random_normal([10]))
+
+layer3 = tf.nn.softmax(tf.matmul(layer2,W3)+b3)
+W4 = tf.Variable(tf.random_normal([10, nb_classes]))
+b4 = tf.Variable(tf.random_normal([nb_classes]))
+
+#hypothesis == softmax
+hypothesis = tf.nn.softmax(tf.matmul(layer3,W4)+b4)
+
+cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis=1))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+#Test model
+is_correct = tf.equal(tf.arg_max(hypothesis,1), tf.arg_max(Y,1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+#parameters
+training_epochs = 15
+batch_size = 100
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    #Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(mnist.train.num_examples / batch_size)
+
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            c, _ = sess.run([cost, optimizer], feed_dict={
+                    X: batch_xs , Y: batch_ys})
+            avg_cost += c / total_batch
+        
+        print('Epoch:', '%04d' % (epoch + 1),
+                'cost:', '{:.9f}'.format(avg_cost))
+    print("Learning finished")
+
+    print("Accuracy:", accuracy.eval(session=sess, feed_dict={
+        X: mnist.test.images, Y: mnist.test.labels}))
+
+    #Get one and predict
+    r = random.randint(0, mnist.test.num_examples - 1)
+    print("Label : ", sess.run(tf.argmax(mnist.test.labels[r:r+1], 1)))
+    print("Prediction : ", sess.run(
+        tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+    
+    plt.imshow(
+        mnist.test.images[r:r+1].reshape(28,28), #image pixel size
+        cmap='Greys',
+        interpolation='nearest')
+    plt.show()
