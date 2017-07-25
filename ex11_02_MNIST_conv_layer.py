cfc6dad4baf132fc2e6ebf31522a14410d967dda
HardPlant <abc7988se@naver.com>
2017-07-25 04:15:47 -0700

    added MNIST

--- ex11_02_MNIST_conv_layer.py
+++ ex11_02_MNIST_conv_layer.py
@@ -11,10 +11,10 @@ plt.imshow(img, cmap='gray')
 sess= tf.InteractiveSession()
 img = img.reshape(-1,28,28,1)
 W1 = tf.Variable(tf.random_normal([3,3,1,5], stddev =0.01))
-conv2d = tf.nn.conv2d(img, W1,strides[1,2,2,1], padding='SAME')
+conv2d = tf.nn.conv2d(img, W1,strides=[1,2,2,1], padding='SAME')
 print(conv2d)
 sess.run(tf.global_variables_initializer())
 conv2d_img = conv2d.eval()
 conv2d_img=np.swapaxes(conv2d_img,0,3)
-for i, one_img in enumerate(conv2d):
+for i, one_img in enumerate(conv2d_img):
     plt.subplot(1,5,i+1), plt.imshow(one_img.reshape(14,14),cmap='gray')
