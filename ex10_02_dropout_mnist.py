5d46be105bea359646303551b7d2a764c64d3cd9
HardPlant <abc7988se@naver.com>
2017-07-25 02:47:51 -0700

    asdf

--- ex10_02_dropout_mnist.py
+++ ex10_02_dropout_mnist.py
@@ -77,7 +77,7 @@ with tf.Session() as sess:
     r = random.randint(0, mnist.test.num_examples - 1)
     print("Label : ", sess.run(tf.argmax(mnist.test.labels[r:r+1], 1)))
     print("Prediction : ", sess.run(
-        tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+        tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1], keep_prob:1}))
     
     plt.imshow(
         mnist.test.images[r:r+1].reshape(28,28), #image pixel size
