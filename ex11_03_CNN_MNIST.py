02e1aed9ddb8e829fe929ec350fb3d0a5586df07
HardPlant <abc7988se@naver.com>
2017-07-25 04:48:03 -0700

    CNN 1

--- ex11_03_CNN_MNIST.py
+++ ex11_03_CNN_MNIST.py
@@ -29,15 +29,41 @@ L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 L2 = tf.reshape(L2, [-1,7*7*64]) # unfold, (?, 3136) <- (?, 784)
 
-W3 = tf.get_varialbe("W2", shape=[7*7*64,10],
+W3 = tf.get_variable("W2", shape=[7*7*64,10],
     initializer = tf.contrib.layers.xavier_initializer())
-b = tf.Variable(tf. random_normal([10]))
+b = tf.Variable(tf. random_normal([10])) # of out
 hypothesis = tf.matmul(L2,W3) + b
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=hypothesis, labels=Y))
-optimizer = tf.train.AdamOptimier(learning_rate=1e-5).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(cost)
 
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+#parameters
+training_epochs = 15
+batch_size = 100
+
+print('Start')
+training_epochs = 15
+batch_size = 10
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        c, _, = sess.run([cost, optimizer], feed_dict = feed_dict)
+        avg_cost += c/total_batch
+    print('Epoch:','%04d'%(epoch+1), 'cost=','{:.9f}'.format(avg_cost))
+
+print('Finished!')
+
+correct_prediction = tf.equal(tf.argmax(hypothesis,1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+    X:mnist.test.images,Y:mnist.test.labels}))
 
 
 
