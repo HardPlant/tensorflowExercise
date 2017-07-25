9f6fd881db899e8f39182f2f508a0e939da99c81
HardPlant <abc7988se@naver.com>
2017-07-25 06:59:52 -0700

    dense layer

--- ex12_02_tf_layers.py
+++ ex12_02_tf_layers.py
@@ -37,26 +37,22 @@ class Model:
             pool3 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], padding='SAME', strides=2)
             dropout3 = tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
 
-            #flat
+            #flat & dense
             flat = tf.reshape(dropout3, [-1,128,4*4])
             dense4 = tf.layers.dense(input=flat, units=625, activation=tf.nn.relu)
             dropout4 = tf.layers.dropout(dense4, rate=0.5, training=self.training)
 
+            self.logits = tf.layers.dense(inputs=dropout4, units=10)
 
-            W5 = tf.get_variable("W5", shape=[625, 10],
-                initializer = tf.contrib.layers.xavier_initializer())
-            b5 = tf.Variable(tf. random_normal([10])) # of out
-            self.logits = tf.matmul(L4,W5) + b5
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=self.logits, labels=self.Y))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).\
+                minimize(self.cost)
 
-            self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-                logits=self.logits, labels=self.Y))
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).\
-                    minimize(self.cost)
-
-            self.correct_prediction = tf.equal(tf.argmax(self.logits,1),
-            tf.argmax(self.Y,1))
-            self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction,
-            tf.float32))
+        self.correct_prediction = tf.equal(tf.argmax(self.logits,1),
+        tf.argmax(self.Y,1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction,
+        tf.float32))
 
     def predict(self, x_test, training=1.0):
         return self.sess.run(self.logits,
