86855b145bac3bd48debbbd275a613c0fada11b0
HardPlant <abc7988se@naver.com>
2017-07-25 05:05:23 -0700

    error expunged

--- ex11_04_Deep_CNN.py
+++ ex11_04_Deep_CNN.py
@@ -43,13 +43,13 @@ L3 = tf.nn.conv2d(L2, W3, strides=[1,1,1,1], padding='SAME')
 L3 = tf.nn.relu(L3)
 L3 = tf.nn.max_pool(L3, ksize=[1,2,2,1], strides=[1,2,2,1],
     padding='SAME')
-L3 = tf.nn.dropout(32, keep_prob=keep_prob)
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 L3 = tf.reshape(L3, [-1,128*4*4]) # unfold, (?, 3136) <- (?, 784)
 
-W4 = tf.get_variable("W4", shape=[4*4*64,625],
+W4 = tf.get_variable("W4", shape=[128*4*4,625],
     initializer = tf.contrib.layers.xavier_initializer())
 b4 = tf.Variable(tf. random_normal([625])) # of out
-L4 = tf.nn.relu(tf.matmul(L3, L4) + b4)
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
 L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
 W5 = tf.get_variable("W5", shape=[625, 10],
@@ -76,7 +76,7 @@ for epoch in range(training_epochs):
     total_batch = int(mnist.train.num_examples / batch_size)
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob:1}
+        feed_dict = {X: batch_xs, Y: batch_ys}
         c, _, = sess.run([cost, optimizer], feed_dict = feed_dict)
         avg_cost += c/total_batch
     print('Epoch:','%04d'%(epoch+1), 'cost=','{:.9f}'.format(avg_cost))
