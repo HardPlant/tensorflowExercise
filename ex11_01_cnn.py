cfd12e68e2653b41209b300bec5b8040a483bb42
HardPlant <abc7988se@naver.com>
2017-07-25 04:02:37 -0700

    SAME padding

--- ex11_01_cnn.py
+++ ex11_01_cnn.py
@@ -11,10 +11,10 @@ print("image.shape", image.shape)
 weight = tf.constant([[[[1.]],[[1.]]],
                       [[[1.]],[[1.]]]])
 print("weight.shape", weight.shape)
-conv2d = tf.nn.conv2d(image, weight, strides=[1,1,1,1], padding = 'VALID')
+conv2d = tf.nn.conv2d(image, weight, strides=[1,1,1,1], padding = 'SAME') # input == output
 conv2d_img = conv2d.eval()
 print("conv2d_img.shape", conv2d_img.shape)
 conv2d_img = np.swapaxes(conv2d_img,0,3)
 for i, one_img in enumerate(conv2d_img):
-    print(one_img.reshape(2,2))
-    plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(2,2),cmap='gray')
+    print(one_img.reshape(3,3))
+    plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(3,3),cmap='gray')
