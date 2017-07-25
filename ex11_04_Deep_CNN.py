ae4756e7460307595e32b373e85983855c2f8d9e
HardPlant <abc7988se@naver.com>
2017-07-25 04:55:12 -0700

    2 layer added

--- ex11_04_Deep_CNN.py
+++ ex11_04_Deep_CNN.py
@@ -8,6 +8,9 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 # more information about the mnist dataset
 img = mnist.train.images[0].reshape(28,28)
 
+#for dropout
+keep_prob = 0.7
+
 X = tf.placeholder(tf.float32, [None, 784])
 X_img = tf.reshape(X, [-1,28,28,1]) # n=-1, 28*28*color=1
 Y = tf.placeholder(tf.float32, [None, 10])
@@ -21,19 +24,38 @@ L1 = tf.nn.relu(L1)
 L1 = tf.nn.max_pool(L1, ksize=[1,2,2,1],
     strides=[1,2,2,1], padding='SAME')
 #L2 ImgIn shape=(?,14,14,32)
+#Let's drop out :)
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
 W2 = tf.Variable(tf.random_normal([3,3,32,64],stddev=0.01))#64 filters
 #conv -> (?, 14, 14, 64)
 #pool -> (?,7,7,64)
 L2 = tf.nn.conv2d(L1, W2, strides=[1,1,1,1], padding='SAME')
 L2 = tf.nn.relu(L2)
-L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
-L2 = tf.reshape(L2, [-1,7*7*64]) # unfold, (?, 3136) <- (?, 784)
+L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides=[1,2,2,1],
+    padding='SAME')
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-W3 = tf.get_variable("W2", shape=[7*7*64,10],
+W3 = tf.Variable(tf.random_normal([3,3,64,128],stddev=0.01))#64 filters
+#conv -> (?, 14, 14, 64)
+#pool -> (?,7,7,64)
+L3 = tf.nn.conv2d(L2, W3, strides=[1,1,1,1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1,2,2,1], strides=[1,2,2,1],
+    padding='SAME')
+L3 = tf.nn.dropout(32, keep_prob=keep_prob)
+L3 = tf.reshape(L3, [-1,128*4*4]) # unfold, (?, 3136) <- (?, 784)
+
+W4 = tf.get_variable("W4", shape=[4*4*64,625],
     initializer = tf.contrib.layers.xavier_initializer())
-b = tf.Variable(tf. random_normal([10])) # of out
-hypothesis = tf.matmul(L2,W3) + b
+b4 = tf.Variable(tf. random_normal([625])) # of out
+L4 = tf.nn.relu(tf.matmul(L3, L4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
+
+W5 = tf.get_variable("W5", shape=[625, 10],
+    initializer = tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf. random_normal([10])) # of out
+hypothesis = tf.matmul(L4,W5) + b5
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=hypothesis, labels=Y))
