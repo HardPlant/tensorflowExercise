4d6370667b6e37ac91059f277a5e2b40500f6ac4
HardPlant <abc7988se@naver.com>
2017-07-25 21:21:28 -0700

    MinMaxScaler

--- ex13_07_stock.py
+++ ex13_07_stock.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 import numpy as np
 
+def MinMaxScaler(data):
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data,0) - np.min(data,0)
+    #noise term prevents the zero division
+    return numerator / (denominator + 1e-7)
+
 timesteps = seq_length = 7
 data_dim = 5
 output_dim = 1
