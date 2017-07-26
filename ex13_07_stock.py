f298052297e679db117b6b0ff7ac50714bfb5092
HardPlant <abc7988se@naver.com>
2017-07-26 05:10:34 -0700

    stock completed

--- ex13_07_stock.py
+++ ex13_07_stock.py
@@ -22,27 +22,40 @@ dataY = []
 for i in range(0, len(y) - seq_length):
     _x = x[i:i + seq_length]
     _y = y[i+seq_length]
-    print(_x, "->", _y)
     dataX.append(_x)
     dataY.append(_y)
+
 #Training & Test
 train_size = int(len(dataY)*0.7)
 test_size = len(dataY) - train_size
-trainX, testX = np.array(dataX[0:train_size]),
+trainX, testX = np.array(dataX[0:train_size]),\
                 np.array(dataX[train_size:len(dataX)])
-trainY, testY = np.array(dataY[0:train_size]),
+trainY, testY = np.array(dataY[0:train_size]),\
                 np.array(dataY[train_size:len(dataY)])
 
 X = tf.placeholder(tf.float32, [None, seq_length, data_dim])
 Y = tf.placeholder(tf.float32, [None, 1])
-
+hidden_dim = 5
 cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_dim, state_is_tuple=True)
 outputs, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
-Y=pred = tf.contrib.layers.fully_connected(
+Y_pred = tf.contrib.layers.fully_connected(
     outputs[:,-1], output_dim, activation_fn = None)
 
 loss = tf.reduce_sum(tf.square(Y_pred - Y)) # sum of the squares
 optimizer = tf.train.AdamOptimizer(0.01)
 train = optimizer.minimize(loss)
 
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
+    for i in range(1000):
+        _, l = sess.run([train, loss],
+            feed_dict = {X: trainX, Y:trainY})
+        print(i, l)
+
+    testPredict = sess.run(Y_pred, feed_dict = {X:testX})
+
+    import matplotlib.pyplot as plt
+    plt.plot(testY)
+    plt.plot(testPredict)
+    plt.show()
