9526d103ead466ece96c5a52cca5546f9ac4a608
HardPlant <abc7988se@naver.com>
2017-07-25 21:06:47 -0700

    init

--- ex13_06_dynamic_rnn.py
+++ ex13_06_dynamic_rnn.py
@@ -1,58 +1,25 @@
 import tensorflow as tf
 import numpy as np
+#shape(1,5,4) = [[[1,0,0,0(x4)],...(x5)]]
+h = [1,0,0,0]
+e = [0,1,0,0]
+l = [0,0,1,0]
+o = [0,0,0,1]
 
-#input = 5 (demension(hello))
-#seq = 6(len(hihello) -1 )
-#hidden(output_demension) = 5 (one hot)
-#batch = 1(string.num)
+hidden_size = 2
+sess = tf.Session()
 
-hidden_size = 5
-input_dim = 5
-batch_size = 1
-sequence_length = 6
+#batching input
+x_data = np.array([[h,e,l,l,o],
+                   [e,o,l,l,l],
+                   [l,l,e,e,l],], dtype= np.float32)
+print(x_data.shape)
+print(x_data)
 
-idx2char = ['h','i','e','l','o']
-x_data = [[0,1,0,2,3,3]] # hihell
-x_one_hot = [[[1,0,0,0,0], #h 0
-              [0,1,0,0,0], #i 1
-              [1,0,0,0,0], #h 0
-              [0,0,1,0,0], #e 2
-              [0,0,0,1,0], #l 3
-              [0,0,0,1,0] #l 3
-              ]]
-y_data = [[1,0,2,3,3,4]] # ihello
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size,
+                                     state_is_tuple=True)
+outputs, _states = tf.nn.dynamic_rnn(cell, x_data, dtype=tf.float32)
 
-#X one-hot, hidden_size = input_demension
-X = tf.placeholder(tf.float32, [None, sequence_length,input_dim])
-#Y label
-Y = tf.placeholder(tf.int32, [None, sequence_length])
-
-cell = tf.contrib.rnn.BasicLSTMCell(
-    num_units=hidden_size, state_is_tuple=True)
-initial_state = cell.zero_state(batch_size, tf.float32)
-outputs, _states = tf.nn.dynamic_rnn(
-    cell,X,initial_state=initial_state,dtype=tf.float32)
-
-#[batch_size * sequence_length]
-weights = tf.ones([batch_size, sequence_length])
-
-#output should not directly here, but for simple example
-sequence_loss = tf.contrib.seq2seq.sequence_loss(
-    logits=outputs, targets=Y, weights=weights)
-
-loss = tf.reduce_mean(sequence_loss)
-#will optimize weights for logit function, target is Y
-train = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
-
-prediction = tf.argmax(outputs, axis=2)
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(2000):
-        l, _ = sess.run([loss, train], feed_dict={X:x_one_hot, Y:y_data})
-        result = sess.run(prediction, feed_dict={X:x_one_hot})
-        if i%10 == 0:
-            print(i, "loss:", l, "prediction:", result, "true Y:", y_data)
-            result_str = [idx2char[c] for c in np.squeeze(result)]
-            print("\tPrediction str:", ''.join(result_str))
-    
+sess.run(tf.global_variables_initializer())
+#will be (1,5,2)
+print(outputs.eval(session=sess))
